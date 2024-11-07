65ec6b481c23add068c909636eaaf8666d70e267
angel452 <angelloayzah2016@gmail.com>
2024-11-06 19:51:52 -0500

    Add multiple components to AppComponent imports for enhanced functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from "./new-component/new-component.component";
+import { InterpolationComponentComponent } from "./interpolation-component/interpolation-component.component";
+import { PadreComponentComponent } from "./padre-component/padre-component.component";
+import { MostrarMsgComponent } from "./mostrar-msg/mostrar-msg.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
+import { CondicionalesIfElseComponent } from "./condicionales-if-else/condicionales-if-else.component";
+import { LocalReferenceComponent } from "./local-reference/local-reference.component";
+import { BucleForComponent } from "./bucle-for/bucle-for.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponentComponent, InterpolationComponentComponent, PadreComponentComponent, MostrarMsgComponent, ReplicadorComponent, CondicionalesIfElseComponent, LocalReferenceComponent, BucleForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '001-appTest';
+  title = 'MAIN COMPONENT';
 }
