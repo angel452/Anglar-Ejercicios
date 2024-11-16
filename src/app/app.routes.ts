13811e6392fb9e1a26b24940c42f7fc34a213825
angel452 <angelloayzah2016@gmail.com>
2024-11-16 16:29:42 -0500

    Add routing for ListaUsuariosComponent and MostrarMsgComponent; configure child routes for PadreComponent and HijoComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
+import { MostrarMsgComponent } from './mostrar-msg/mostrar-msg.component';
+import { PadreComponentComponent } from './padre-component/padre-component.component';
+import { HijoComponent } from './padre-component/hijo/hijo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ListaUsuariosComponent},
+    {path: 'mostrar-mensaje', component: MostrarMsgComponent},
+    {
+        path: 'configuracion', children: [
+            {path: 'padre', component: PadreComponentComponent},
+            {path: 'hijo', component: HijoComponent}
+        ]
+    }
+
+];
